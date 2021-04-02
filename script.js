3188ba5d1a1070d2a852fa35d7e298b5fe3d99f0
Damilola <dami_fagbo@yahoo.com>
2021-04-02 14:47:16 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,63 +1,25 @@
-const regForm = document.getElementById('reg_form');
-const loginForm = document.getElementById('login_form');
-
-if(regForm){
-    regForm.addEventListener('submit', (evt)=> {
-        evt.preventDefault()
-        register()
-    })
-}
-
-if(loginForm){
-    loginForm.addEventListener('submit', (evt)=> {
-        evt.preventDefault()
-        check()
-    })
-  }
+const regForm = document.getElementById('reg_btns');
+const loginForm = document.getElementById('login_btns');
+const nameForm = document.getElementById('name-form');
 
 
 //Select all elements
 function register(){ 
-    const mailEl = document.getElementById('mail').value;
-    const pwEl = document.getElementById('psw').value;
-    const userNameEl = document.getElementById('uname').value;
-    const numEl = document.getElementById('num').value
-    /* let mail = mailEl.value;
-    let pw = pwEl.value;
-    let num = numEl.value;
-    let userName = userNameEl.value; */
-
+    const mail = document.getElementById('mail').value;
+    const pw = document.getElementById('psw').value;
+    const userName = document.getElementById('uname').value;
+    const num = document.getElementById('num').value;
     
     //storing to local storage
 
-     if(userNameEl === ''){
-    userNameEl.style.border = "1px solid red";
-    userNameEl.focus();
-    alert('Please fill in username');
-    return false;
-}
- if(pw === ''){
-    pwEl.style.border = "1px solid red";
-    alert('Please fill in password');
-    pwEl.focus();
-    return false;
 
-} 
-
- if(!pw.match(num)){
-    pw.style.border = "1px solid red";
-    alert('please add 1 number');
-    return
-
-} 
-
-    localStorage.setItem('mail', mail)
-    localStorage.setItem('psw', pw)
-    localStorage.setItem('num',num)
-    localStorage.setItem('uname', userName)
+    window.localStorage.setItem('mail', mail)
+    window.localStorage.setItem('psw', pw)
+    window.localStorage.setItem('num',num)
+    window.localStorage.setItem('uname', userName)
    
 
- document.getElementById('mail').value = "";
+    document.getElementById('mail').value = "";
     document.getElementById('psw').value = "";
     document.getElementById('num').value = "";
     document.getElementById('uname').value = ""; 
@@ -66,12 +28,16 @@ function register(){
     window.location.href ="/login.html";
 } 
 
+if(regForm){
+    regForm.addEventListener('click', (evt)=> {
+        evt.preventDefault()
+        register()
+    })
+}
 
- 
 //checking
 
 function check(){ 
-    let userName = localStorage.getItem('uname')
     let storedMail = localStorage.getItem('mail');
     let storedPw = localStorage.getItem('psw');
 
@@ -81,7 +47,7 @@ function check(){
 
     if(userMail.value == storedMail && userPw.value == storedPw){
         
-        localStorage.setItem('code','secret') 
+        window.localStorage.setItem('code','secret') 
         alert('You are logged in.');
 
         window.location.href ="/profile.html";
@@ -91,24 +57,32 @@ function check(){
     }
     
 }
-
+if(loginForm){
+    loginForm.addEventListener('click', (evt)=> {
+        evt.preventDefault()
+        check()
+    })
+  }
 
 //log out
     function logOut(){ 
+    nameForm.innerHTML = `Hello ${userName}, Welcome to your profile`;
    let secret = localStorage.getItem('code');
    if (secret=='secret'){
    
 }else {
-    window.localStorage.removeItem('uname')
-    window.localStorage.removeItem('mail')
-    window.localStorage.removeItem('num')
-    window.localStorage.removeItem('psw')
+    window.localStorage.clear('uname')
+    window.localStorage.clear('mail')
+    window.localStorage.clear('num')
+    window.localStorage.clear('psw')
     window.location.href="/login.html";
     alert("Logged out successfully!")
     document.classList.remove('container')
     document.style.display = none;
-    }   
+
+    }    
 }
+
 function logoutForm(){
     document.getElementById('logout').addEventListener('click',
     logout()
